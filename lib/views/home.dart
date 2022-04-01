7750d712852300598b60561826916a4dafc1b329
fizabells22 <fizabella.ru@gmail.com>
2022-04-01 23:01:54 +0700

    add categories_model (31:23)

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -18,6 +18,32 @@ class _HomeState extends State<Home> {
         title: brandName(),
         elevation: 0.0,
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(24),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "search wallpaper",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  Icon(Icons.search)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 Widget brandName() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
-    children: [
+    children: <Widget>[
       Text("Wallpaper",
           style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600)),
       Text(
