234d7df3c78e2d1172e67b227129e071e3311fe4
fizabells22 <fizabella.ru@gmail.com>
2022-04-03 21:06:14 +0700

    fix image_view.dart

--- lib/views/image_view.dart
+++ lib/views/image_view.dart
@@ -1,12 +1,15 @@
-import 'dart:ffi';
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ImageView extends StatefulWidget {
   final String imgUrl;
@@ -37,25 +40,22 @@ class _ImageViewState extends State<ImageView> {
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             alignment: Alignment.bottomCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    _save();
-                  },
-                  child: Stack(
-                    children: [
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width/2,
-                      color: Color(0xff1C1B1B).withOpacity(0.8),
-                    ),
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width/2,
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                      decoration: BoxDecoration(
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              GestureDetector(
+                onTap: () {
+                  _save();
+                },
+                child: Stack(children: [
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2,
+                    color: Color(0xff1C1B1B).withOpacity(0.8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2,
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
                         border: Border.all(
                           color: Colors.white54,
                           width: 1.0,
@@ -66,44 +66,45 @@ class _ImageViewState extends State<ImageView> {
                             Color(0x36FFFFFF),
                             Color(0x0FFFFFFF),
                           ],
-                        )
-                      ),
-                      child: Column(
-                        children: [
-                          Text('Set Wallpaper',
-                            style: TextStyle(
-                              fontSize: 10, 
-                              color: Colors.white70,
-                              ),
-                            ),
-                          Text('Image Will be saved in gallery', 
-                            style: TextStyle(
-                              fontSize: 10, 
-                              color: Colors.white70,
-                              ),
-                            ),
-                        ],
-                      ),
+                        )),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Set Wallpaper',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          'Image Will be saved in gallery',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
                     ),
-                    ]
                   ),
-                ),
-                SizedBox(height: 16.0),
-                Text('Cancel', style: TextStyle(color: Colors.white),),
-                SizedBox(height: 50),
-              ]
-            ),
+                ]),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Cancel',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 50),
+            ]),
           )
         ],
       ),
     );
   }
-}
 
- _save() async {
+  _save() async {
     await _askPermission();
-    var response = await Dio().get(widget.imgUrl,
-        options: Options(responseType: ResponseType.bytes));
+    var response = await Dio()
+        .get(widget.imgUrl, options: Options(responseType: ResponseType.bytes));
     final result =
         await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
     print(result);
@@ -113,10 +114,10 @@ class _ImageViewState extends State<ImageView> {
   _askPermission() async {
     if (Platform.isIOS) {
       /*Map<PermissionGroup, PermissionStatus> permissions =
-          */await PermissionHandler()
-              .requestPermissions([PermissionGroup.photos]);
+          */
+      await PermissionHandler().requestPermissions([PermissionGroup.photos]);
     } else {
-     /* PermissionStatus permission = */await PermissionHandler()
+      /* PermissionStatus permission = */ await PermissionHandler()
           .checkPermissionStatus(PermissionGroup.storage);
     }
   }
